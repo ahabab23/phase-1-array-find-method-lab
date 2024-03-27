9afed49bfc6a12c829b7465b5b455b57b60e979d
Uwes Ahabab <uwesahabab@gmail.com>
2024-03-27 10:56:51 +0300

    array -find method

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(array){
+    for(let item of array){
+        if(item.result === "W"){
+            return item["year"];
+        }
+    }
+    
+}
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+]
+record.find(superbowlWin)
+
+
